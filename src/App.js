0b35c8f720291c3437c1ee2ad1cc4de303735d27
leenahmad <leenkaraja24@gmail.com>
2022-04-27 22:40:12 +0300

    lab 27 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 
-
-import './App.css';
+import './App.scss';
 
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Form from './components/form/form';
 import Results from './components/results/results';
 
-class App extends React.Component{
+function App(){
+  const [data , setData] = useState(null);
+  const [requestparams , setReq] = useState({});
 
-  render(){
+ const callApi = async (requestparams) =>{
+    
+  setReq(requestparams)
+  // let callMethod =  requestparams.method
+  const res = await axios (requestparams.url , (requestparams.body) ? (requestparams.body) : null)
+    
+    const data = {
+      results: res.data
+
+    }
+    setData(data)
+
+}
+
+
     return(
       <React.Fragment>
       <Header />
-      <Form />
-      <Results />
+
+      <Form handleApiCall= {callApi} />
+      <Results data={data} url={requestparams.url} method={requestparams.method}/>
+
       <Footer />
       </React.Fragment>
     )
   }
-}
+
 
 
 export default App;
